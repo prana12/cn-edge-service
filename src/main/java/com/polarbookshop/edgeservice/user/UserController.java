200903fa97628e68d9cf3566d2e0a73143337c36
Prakash <prana12@live.com>
2026-01-31 00:30:27 +0000

    Chapter13: Configure Logging

--- src/main/java/com/polarbookshop/edgeservice/user/UserController.java
+++ src/main/java/com/polarbookshop/edgeservice/user/UserController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 //import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 //import org.springframework.security.core.context.SecurityContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Mono;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
     /*@GetMapping("user")
     public Mono<User> getUser() {
@@ -32,6 +36,7 @@ public class UserController {
 
     @GetMapping("user")
     public Mono<User> getUser(@AuthenticationPrincipal OidcUser oidcUser) {
+        log.info("Fetching information about the currently authenticated user");
         var user = new User(
                 oidcUser.getPreferredUsername(),
                 oidcUser.getGivenName(),
